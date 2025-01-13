d5c9cc24ad0ccf22260555628b6e59f6540a5177
Florian Klare <INWIA3V8@inwerken.de>
2025-01-13 09:58:10 +0100

    neuer Build

--- dist/controller/Main-dbg.controller.js
+++ dist/controller/Main-dbg.controller.js
@@ -1,29 +1,93 @@
-sap.ui.define(["./BaseController","sap/ui/model/json/JSONModel",], function (BaseController,JSONModel) {
-	"use strict";
+sap.ui.define(
+	["./BaseController", "sap/ui/model/json/JSONModel"],
+	function (BaseController, JSONModel) {
+		"use strict";
 
-	return BaseController.extend("inw.le_scanner.controller.Main", {
-		onInit: function () {
-			const oViewModel = new JSONModel({
-				istLagereinheitBarcode:"",
-				wechselLagerplatzBarcode:"",
-				sollLagereinheitBarcode:"",
-				TANummer: "",
-				anzahlPositionen: "",
-				Lagerplatz: "",
-			});
-			this.setModel(oViewModel, "viewModel");
-		
+		return BaseController.extend("inw.le_scanner.controller.Main", {
+			onInit: function () {
+				const oViewModel = new JSONModel({
+					istLagereinheitBarcode: "",
+					istLagerplatzBarcode: "",
+					sollLagereinheitBarcode: "",
+					TANummer: "",
+					anzahlPositionen: "",
+				});
+				this.setModel(oViewModel, "viewModel");
+
+				//focus select
+				const oInputIst = this.byId("istLagereinheitBarcode");
+				const oInputWechsel = this.byId("istLagerplatzBarcode");
+				const oInputSoll = this.byId("sollLagereinheitBarcode");
+
+				const aInputs = [oInputIst, oInputWechsel, oInputSoll];
+
+				aInputs.forEach((oInput) => {
+					oInput.addEventDelegate({
+						onfocusin: this.onFocus.bind(this),
+						onfocusout: this.onFocusOut.bind(this),
+					});
+				});
+
+				// stop keyboard popup
+				aInputs.forEach((oInput) => {
+					oInput.addEventDelegate({
+						onAfterRendering: function () {
+							const oDomRef = oInput.getDomRef();
+							if (oDomRef) {
+								oDomRef.setAttribute("inputmode", "none");
+							}
+						}
+					});
+				});
+
+			},
+
+			onAfterRendering: function () {},
+
+			onFocus: function (oEvent) {
+				// Get the source control of the focus event
+				const oInput = oEvent.srcControl;
+
+				// Get the DOM reference of the input field
+				const oDomRef = oInput.getDomRef("inner");
+
+				// Select the text in the input field if the DOM element exists
+				if (oDomRef) {
+					oDomRef.select();
+				}
+			},
+			onFocusOut: function (oEvent) {
+				const oInput = oEvent.getSource();
+
+				// Deactivate virtual keyboard 
+				oInput.addEventDelegate({
+					onAfterRendering: function () {
+						const oDomRef = oInput.getDomRef();
+						if (oDomRef) {
+							oDomRef.setAttribute("inputmode", "none");
+						}
+					}
+				});
+
+			},
+
+			onKeyboardAction: function (oEvent) {
+				const oInput = oEvent.getSource();
+
+				// Activate virtual keyboard 
+				oInput.addEventDelegate({
+					onAfterRendering: function () {
+						const oDomRef = oInput.getDomRef();
+						if (oDomRef) {
+							oDomRef.setAttribute("inputmode", "text");
+						}
+					}
+				});
+				oInput.focus();
 			
-		},
-		onAfterRendering: function (){
-			const oTANummer = this.byId("TANummer");
-			const oAnzahlPositionen = this.byId("anzahlPositionen");
-			const oLagerplatz = this.byId("Lagerplatz");
-			   // Füge das Delegate hinzu, um nach dem Rendern den tabindex zu setzen
-			   oTANummer.addEventDelegate;
-			   oAnzahlPositionen.addEventDelegate;
-			   oLagerplatz.addEventDelegate;
-		},
-		
-	});
-});
+			},
+
+			
+		});
+	}
+);
