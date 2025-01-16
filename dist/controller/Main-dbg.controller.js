43a184a2a099a8b7bb913c61687e1cb6be73d511
Florian Klare <INWIA3V8@inwerken.de>
2025-01-16 09:48:59 +0100

    build

--- dist/controller/Main-dbg.controller.js
+++ dist/controller/Main-dbg.controller.js
@@ -80,7 +80,13 @@ sap.ui.define(['./BaseController', 'sap/ui/model/json/JSONModel', 'sap/m/Message
         //   this.onP2();
         //   break;
         default:
-          MessageToast.show("Key pressed: ", oEvent.key, " | KeyCode: ", oEvent.keyCode);
+          MessageToast.show("Key pressed: " + oEvent.key + " | KeyCode: " + oEvent.keyCode, {
+            duration: 3000, // Optional: Wie lange der Toast angezeigt wird (in Millisekunden)
+            my: "center center", // Positioniert den Toast in der Mitte des Bildschirms
+            at: "center center", // Zeigt den Toast in der Mitte des Bildschirms
+            of: window, // Definiert, dass der Toast im Fenster (browser) angezeigt wird
+            offset: "0 0" // Verhindert eine Verschiebung
+        });
       }
     },
 
