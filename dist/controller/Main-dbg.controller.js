e14bbfd99713f502cc7a4a31d6c3b22eae00a702
Florian Klare <INWIA3V8@inwerken.de>
2025-01-17 08:48:40 +0100

    build

--- dist/controller/Main-dbg.controller.js
+++ dist/controller/Main-dbg.controller.js
@@ -175,7 +175,7 @@ sap.ui.define(['./BaseController', 'sap/ui/model/json/JSONModel', 'sap/m/Message
       // Validierung
 
       MessageToast.show('Erfolgreich gebucht!');
-
+      
       // hier wird die Lagereinheit und Lagerplatz ins Backend geschickt
 
       // Model leeren und wieder ins erste Feld springen
@@ -195,6 +195,7 @@ sap.ui.define(['./BaseController', 'sap/ui/model/json/JSONModel', 'sap/m/Message
       // if (err){
       //   this.errorSound.play();
       // }
+      this.successSound.play();
 
     },
 
@@ -244,7 +245,6 @@ sap.ui.define(['./BaseController', 'sap/ui/model/json/JSONModel', 'sap/m/Message
         oViewModel.setProperty('/istLagerplatzBarcodeState', 'Success');
         oViewModel.setProperty('/istLagerplatzBarcodeStateText', 'Soll = Ist Nachlagerplatz');
         oViewModel.setProperty('/sollLagereinheitBarcodeStateText', 'Soll = Ist Lagereinheit');
-        this.successSound.play();
       } else {
         oViewModel.setProperty('/istLagerplatzBarcodeState', 'Warning');
         oViewModel.setProperty('/istLagerplatzBarcodeStateText', 'Soll ≠ Ist Nachlagerplatz');
@@ -259,7 +259,6 @@ sap.ui.define(['./BaseController', 'sap/ui/model/json/JSONModel', 'sap/m/Message
       if (istLagereinheitBarcode === sollLagereinheitBarcode) {
         oViewModel.setProperty('/sollLagereinheitBarcodeState', 'Success');
         oViewModel.setProperty('/sollLagereinheitBarcodeStateText', 'Soll = Ist Lagereinheit');
-        this.successSound.play();
       } else {
         oViewModel.setProperty('/sollLagereinheitBarcodeState', 'Warning');
         oViewModel.setProperty('/sollLagereinheitBarcodeStateText', 'Soll ≠ Ist Lagereinheit');
